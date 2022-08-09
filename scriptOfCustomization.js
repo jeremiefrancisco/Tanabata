85c4a24953ab9193c5d06f39736d495590e44872
jeremiefrancisco <109307076+jeremiefrancisco@users.noreply.github.com>
2022-08-09 09:31:27 -0700

    Add files via upload

--- scriptOfCustomization.js
+++ scriptOfCustomization.js
@@ -17,9 +17,6 @@ mainCharacter.src = "images/sprites/customizationSprites/male/maleOption.png";
 mainCharacter.onload = function drawBase() {
 ctx2.drawImage(mainCharacter,45,15,600,350);
 }
-//this will save which character desing the user chose
-let characterLevel = 0;
-let characterDesign = "";
 //the two buttons
 let rightButton = new Image();
 rightButton.src = "images/sprites/customizationSprites/buttons/right.png";
@@ -58,47 +55,8 @@ let skinToneMale = ['sprites/customizationSprites/male/maleOption.png','sprites/
                 'sprites/customizationSprites/male/maleOptionSC2.png','sprites/customizationSprites/male/maleOptionSC3.png',
                 'sprites/customizationSprites/male/maleOptionSC4.png'];
 let sMNum = 0;
-//the array that holds the hairstyles for maleBase
-let hairstyleMB = ['sprites/customizationSprites/male/maleOption.png', 'sprites/customizationSprites/male/hairstyle/maleBaseRedHair.png',
-                'sprites/customizationSprites/male/hairstyle/maleBaseMohawk.png'];
-let hairMBNum = 0;
-//the array that holds the hairstyles for male SC1
-let hairstyleMB1 = ['sprites/customizationSprites/male/maleOptionSC1.png', 'sprites/customizationSprites/male/hairstyle/SC1/maleSC1Mohawk.png',
-                'sprites/customizationSprites/male/hairstyle/SC1/maleSC1RedHair.png'];
-let hairMBNum1 = 0;
-//the array that holds the hairstyles for male SC2
-let hairstyleMB2 = ['sprites/customizationSprites/male/maleOptionSC2.png', 'sprites/customizationSprites/male/hairstyle/SC2/maleSC2Mohawk.png',
-                'sprites/customizationSprites/male/hairstyle/SC2/maleSC2RedHair.png'];
-let hairMBNum2 = 0;
-//the array that holds the hairstyles for male SC3
-let hairstyleMB3 = ['sprites/customizationSprites/male/maleOptionSC3.png', 'sprites/customizationSprites/male/hairstyle/SC3/maleSC3Mohawk.png',
-                'sprites/customizationSprites/male/hairstyle/SC3/maleSC3RedHair.png'];
-let hairMBNum3 = 0;
-//the array that holds the hairstyles for male SC4
-let hairstyleMB4 = ['sprites/customizationSprites/male/maleOptionSC4.png', 'sprites/customizationSprites/male/hairstyle/SC4/maleSC4Mohawk.png',
-                'sprites/customizationSprites/male/hairstyle/SC4/maleSC4RedHair.png'];
-let hairMBNum4 = 0;
-//the array that holds the hairstyles for femaleBase
-let hairstyleFMB = ['sprites/customizationSprites/female/femaleOption.png', 'sprites/customizationSprites/female/hairstyle/femaleBasePonytail.png',
-                'sprites/customizationSprites/female/hairstyle/femaleBaseDoublePonytail.png'];
-let hairFMBNum = 0;
-//the array that holds the hairstyles for female SC1
-let hairstyleFMB1 = ['sprites/customizationSprites/female/femaleOptionSC1.png', 'sprites/customizationSprites/female/hairstyle/SC1/femaleSC1Ponytail.png',
-                'sprites/customizationSprites/female/hairstyle/SC1/femaleSC1DoublePonytail.png'];
-let hairFMBNum1 = 0;
-//the array that holds the hairstyles for female SC2
-let hairstyleFMB2 = ['sprites/customizationSprites/female/femaleOptionSC2.png', 'sprites/customizationSprites/female/hairstyle/SC2/femaleSC2Ponytail.png',
-                'sprites/customizationSprites/female/hairstyle/SC2/femaleSC2DoublePonytail.png'];
-let hairFMBNum2 = 0;
-//the array that holds the hairstyles for female SC3
-let hairstyleFMB3 = ['sprites/customizationSprites/female/femaleOptionSC3.png', 'sprites/customizationSprites/female/hairstyle/SC3/femaleSC3Ponytail.png',
-                'sprites/customizationSprites/female/hairstyle/SC3/femaleSC3DoublePonytail.png'];
-let hairFMBNum3 = 0;
-//the array that holds the hairstyles for female SC4
-let hairstyleFMB4 = ['sprites/customizationSprites/female/femaleOptionSC4.png', 'sprites/customizationSprites/female/hairstyle/SC4/femaleSC4Ponytail.png',
-              'sprites/customizationSprites/female/hairstyle/SC4/femaleSC4DoublePonytail.png'];
-let hairFMBNum4 = 0;
-//functions that controls the which base style is shown
+
+//functions that controls which base style is shown
 function next() {
   if(userEnterInput == false) {
 //level to change the gender
@@ -111,7 +69,7 @@ function next() {
   rightB();
 //level to change skin tones
 } else if(customizationLevel == 1) {
-//female skin tone 0
+//female skin tone
   if(bNum == 1) {
   ctx2.clearRect(0,0,700,450);
   if ( sFNum >= skinToneFemale.length-1)  sFNum = -1;
@@ -128,104 +86,6 @@ function next() {
   changeSkinToneMale();
   leftB();
   rightB();
-}
-//level to change hairstyle
-} else if(customizationLevel == 2) {
-//female 0 hairstyle
-  if(bNum == 1 && sFNum == 0) {
-  ctx2.clearRect(0,0,700,450);
-  if ( hairFMBNum >= hairstyleFMB.length-1)  hairFMBNum = -1;
-  hairFMBNum++;
-  changeBaseFemaleHair();
-  leftB();
-  rightB();
-  characterLevel = 1;
-//female 1 hairstyle
-} else if(bNum == 1 && sFNum == 1) {
-  ctx2.clearRect(0,0,700,450);
-  if ( hairFMBNum1 >= hairstyleFMB1.length-1)  hairFMBNum1 = -1;
-  hairFMBNum1++;
-  changeFemale1Hair();
-  leftB();
-  rightB();
-  characterLevel = 2;
-//female 2 hairstyle
-} else if(bNum == 1 && sFNum == 2) {
-  ctx2.clearRect(0,0,700,450);
-  if ( hairFMBNum2 >= hairstyleFMB2.length-1)  hairFMBNum2 = -1;
-  hairFMBNum2++;
-  changeFemale2Hair();
-  leftB();
-  rightB();
-  characterLevel = 3;
-//female 3 hairstyle
-} else if(bNum == 1 && sFNum == 3) {
-  ctx2.clearRect(0,0,700,450);
-  if ( hairFMBNum3 >= hairstyleFMB3.length-1)  hairFMBNum3 = -1;
-  hairFMBNum3++;
-  changeFemale3Hair();
-  leftB();
-  rightB();
-  characterLevel = 4;
-//female 4 hairstyle
-} else if(bNum == 1 && sFNum == 4) {
-  ctx2.clearRect(0,0,700,450);
-  if ( hairFMBNum4 >= hairstyleFMB4.length-1)  hairFMBNum4 = -1;
-  hairFMBNum4++;
-  changeFemale4Hair();
-  leftB();
-  rightB();
-  characterLevel = 5;
-}
-//male 0 hairstyle
-  else if(bNum == 0 && sMNum == 0) {
-  ctx2.clearRect(0,0,700,450);
-  if ( hairMBNum >= hairstyleMB.length-1)  hairMBNum = -1; {
-  hairMBNum++;
-  changeBaseMaleHair();
-  leftB();
-  rightB();
-}
-}
-//male 1 hairstyle
-else if(bNum == 0 && sMNum == 1) {
-ctx2.clearRect(0,0,700,450);
-if ( hairMBNum1 >= hairstyleMB1.length-1)  hairMBNum1 = -1; {
-hairMBNum1++;
-changeMale1Hair();
-leftB();
-rightB();
-}
-}
-//male 2 hairstyle
-else if(bNum == 0 && sMNum == 2) {
-ctx2.clearRect(0,0,700,450);
-if ( hairMBNum2 >= hairstyleMB2.length-1)  hairMBNum2 = -1; {
-hairMBNum2++;
-changeMale2Hair();
-leftB();
-rightB();
-}
-}
-//male 3 hairstyle
-else if(bNum == 0 && sMNum == 3) {
-ctx2.clearRect(0,0,700,450);
-if ( hairMBNum3 >= hairstyleMB3.length-1)  hairMBNum3 = -1; {
-hairMBNum3++;
-changeMale3Hair();
-leftB();
-rightB();
-}
-}
-//male 3 hairstyle
-else if(bNum == 0 && sMNum == 4) {
-ctx2.clearRect(0,0,700,450);
-if ( hairMBNum4 >= hairstyleMB4.length-1)  hairMBNum4 = -1; {
-hairMBNum4++;
-changeMale4Hair();
-leftB();
-rightB();
-}
 }
 }
 }
@@ -250,6 +110,7 @@ function previous(){
   changeSkinToneFemale();
   rightB();
   leftB();
+  console.log(characterDesign)
 }
 //male skin tones
 else {
@@ -260,99 +121,9 @@ else {
   rightB();
   leftB();
 }
-//level to change the hairstyle
-} else if(customizationLevel == 2) {
-//female 0 hairstyles
-  if(bNum == 1 && sFNum == 0) {
-  ctx2.clearRect(0,0,700,450);
-  if (hairFMBNum <= 0) hairFMBNum = hairstyleFMB.length;
-  hairFMBNum--;
-  changeBaseFemaleHair();
-  rightB();
-  leftB();
-//female 1 hairstyle
-} else if(bNum == 1 && sFNum == 1) {
-  ctx2.clearRect(0,0,700,450);
-  if (hairFMBNum1 <= 0) hairFMBNum1 = hairstyleFMB1.length;
-  hairFMBNum1--;
-  changeFemale1Hair();
-  rightB();
-  leftB();
-//female 2 hairstyle
-} else if(bNum == 1 && sFNum == 2) {
-  ctx2.clearRect(0,0,700,450);
-  if (hairFMBNum2 <= 0) hairFMBNum2 = hairstyleFMB2.length;
-  hairFMBNum2--;
-  changeFemale2Hair();
-  rightB();
-  leftB();
-//female 3 hairstyle
-} else if(bNum == 1 && sFNum == 3) {
-  ctx2.clearRect(0,0,700,450);
-  if (hairFMBNum3 <= 0) hairFMBNum3 = hairstyleFMB3.length;
-  hairFMBNum3--;
-  changeFemale3Hair();
-  rightB();
-  leftB();
-//female 4 hairstyle
-} else if(bNum == 1 && sFNum == 4) {
-  ctx2.clearRect(0,0,700,450);
-  if (hairFMBNum4 <= 0) hairFMBNum4 = hairstyleFMB4.length;
-  hairFMBNum4--;
-  changeFemale4Hair();
-  rightB();
-  leftB();
-}
-//male 0 hairstyles
-else if(bNum == 0 && sMNum == 0) {
-  ctx2.clearRect(0,0,700,450);
-  if (hairMBNum <= 0) hairMBNum = hairstyleMB.length;
-  hairMBNum--;
-  changeBaseMaleHair();
-  rightB();
-  leftB();
-}
-//male 1 hairstyles
-else if(bNum == 0 && sMNum == 1) {
-  ctx2.clearRect(0,0,700,450);
-  if (hairMBNum1 <= 0) hairMBNum1 = hairstyleMB1.length;
-  hairMBNum1--;
-  changeMale1Hair();
-  rightB();
-  leftB();
-}
-//male 2 hairstyles
-else if(bNum == 0 && sMNum == 2) {
-  ctx2.clearRect(0,0,700,450);
-  if (hairMBNum2 <= 0) hairMBNum2 = hairstyleMB2.length;
-  hairMBNum2--;
-  changeMale2Hair();
-  rightB();
-  leftB();
-}
-//male 3 hairstyles
-else if(bNum == 0 && sMNum == 3) {
-  ctx2.clearRect(0,0,700,450);
-  if (hairMBNum3 <= 0) hairMBNum3 = hairstyleMB3.length;
-  hairMBNum3--;
-  changeMale3Hair();
-  rightB();
-  leftB();
-}
-//male 4 hairstyles
-else if(bNum == 0 && sMNum == 4) {
-  ctx2.clearRect(0,0,700,450);
-  if (hairMBNum4 <= 0) hairMBNum4 = hairstyleMB4.length;
-  hairMBNum4--;
-  changeMale4Hair();
-  rightB();
-  leftB();
 }
 }
 }
-}
-
-
 //changes between male and female
 function changeBase() {
   return mainCharacter.setAttribute('src','images/'+base[bNum]);
@@ -365,46 +136,6 @@ function changeSkinToneFemale() {
 function changeSkinToneMale() {
   return mainCharacter.setAttribute('src','images/'+skinToneMale[sMNum]);
 }
-//changes between female 0 hairstyles
-function changeBaseFemaleHair() {
-  return mainCharacter.setAttribute('src','images/'+hairstyleFMB[hairFMBNum]);
-}
-//changes between female 1 hairstyles
-function changeFemale1Hair() {
-  return mainCharacter.setAttribute('src','images/'+hairstyleFMB1[hairFMBNum1]);
-}
-//changes between female 2 hairstyles
-function changeFemale2Hair() {
-  return mainCharacter.setAttribute('src','images/'+hairstyleFMB2[hairFMBNum2]);
-}
-//changes between female 3 hairstyles
-function changeFemale3Hair() {
-  return mainCharacter.setAttribute('src','images/'+hairstyleFMB3[hairFMBNum3]);
-}
-//changes between female 4 hairstyles
-function changeFemale4Hair() {
-  return mainCharacter.setAttribute('src','images/'+hairstyleFMB4[hairFMBNum4]);
-}
-//changes between male 0 skintones
-function changeBaseMaleHair() {
-  return mainCharacter.setAttribute('src','images/'+hairstyleMB[hairMBNum]);
-}
-//changes between male 1 skintones
-function changeMale1Hair() {
-  return mainCharacter.setAttribute('src','images/'+hairstyleMB1[hairMBNum1]);
-}
-//changes between male 2 skintones
-function changeMale2Hair() {
-  return mainCharacter.setAttribute('src','images/'+hairstyleMB2[hairMBNum2]);
-}
-//changes between male 3 skintones
-function changeMale3Hair() {
-  return mainCharacter.setAttribute('src','images/'+hairstyleMB3[hairMBNum3]);
-}
-//changes between male 4 skintones
-function changeMale4Hair() {
-  return mainCharacter.setAttribute('src','images/'+hairstyleMB4[hairMBNum4]);
-}
 //variable will let the program know which level of the customization stage it is at
 let customizationLevel = 0;
 //variable will let the program know which gender
@@ -413,6 +144,8 @@ let chosenGender = 0;
 let chosenSkin = 0;
 //this makes the program stop when you press enter on a character design
 let userEnterInput = false;
+//this will save which character design the user chose
+let characterDesign = "";
 //function that will confirm or deny selections
 function confirm() {
   if(userEnterInput == true) {
@@ -420,7 +153,20 @@ function confirm() {
   userEnterInput = false;
 }
   if(customizationLevel == 2) {
-
+    location.replace("indexOfPart1.html");
+    if(chosenGender == 1) {
+      if(sFNum == 0) {
+        characterDesign = "sprites/customizationSprites/female/femaleOption.png";
+      } else if(sFNum == 1) {
+        characterDesign = "sprites/customizationSprites/female/femaleOptionSC1.png";
+      } else if(sFNum == 2) {
+        characterDesign = "sprites/customizationSprites/female/femaleOptionSC2.png";
+      } else if(sFNum == 3) {
+        characterDesign = "sprites/customizationSprites/female/femaleOptionSC3.png";
+      } else if(sFNum == 4) {
+        characterDesign = "sprites/customizationSprites/female/femaleOptionSC4.png";
+      }
+    }
   }
 }
 function decline() {
@@ -440,11 +186,7 @@ function changeLevel() {
     console.log("Are you sure you want to choose male? Press Y/N")
   }
   } else if(customizationLevel == 1) {
-    console.log("Are you sure you want to choose this skin color? Press Y/N")
-  } else if(customizationLevel == 2) {
-    console.log("Are you sure you want to choose this hairstyle? Press Y/N")
-  } else if(customizationLevel == 3) {
-    console.log("Are you happy with your character?")
+    console.log("Are you happy with this character? Press Y/N")
   }
 }
 //switch case for function keys
--- scriptOfPart1.js
+++ scriptOfPart1.js
@@ -1,55 +1,207 @@
-let canvas = document.getElementById('myCanvas');
-let ctx = canvas.getContext('2d');
+let backgroundImage = document.getElementById('myCanvas');
+let bg = backgroundImage.getContext('2d');
 
-let canvas1 = document.getElementById('myCanvas1');
-let ctx1 = canvas1.getContext('2d');
+let main = document.getElementById('myCanvas1');
+let mc = main.getContext('2d');
 
-let midXCanvas = 350;
-let midYCanvas = 225;
+let opponent = document.getElementById('myCanvas2');
+let opp = opponent.getContext('2d');
 
-ctx.strokeStyle = 'blue';
-ctx.strokeRect(midXCanvas-50, midYCanvas-50, 100, 100);
+let inventory = document.getElementById('myCanvas3');
+let inv = inventory.getContext('2d');
+//bacgkround related coordinates
+let backgroundX = 0;
+let backgroundY = 0;
+let midXCanvas = 320;
+let midYCanvas = 180;
+let canvasWidth = 700;
+let canvasHeight = 450;
+//opponent related coordinates
+let opponentX1 = Math.floor(Math.random()*11)*200;;
+let opponentY1 = 0;
+let opponentX2 = opponentX1+50;
+let opponentY2 = 60;
+console.log(opponentX1)
+//healthbars
+let opponentHealth = 2;
+let mcHealth = 10;
+//mc related coordinates
+let mcPositionX1 = 320;
+let mcPositionY1 = 180;
+let mcPositionX2 = mcPositionX1+50;
+let mcPositionY2 = mcPositionY1+60;
+//background picture
+let backgroundPicture = new Image();
+backgroundPicture.src = "images/testBackground.png";
+backgroundPicture.onload = function drawbackgroundPicture() {
+bg.drawImage(backgroundPicture,backgroundX,backgroundY,canvasWidth,canvasHeight);
+}
+function background() {
+  let backgroundPicture = new Image();
+  backgroundPicture.src = "images/testBackground.png";
+  backgroundPicture.onload = function drawTest() {
+  bg.drawImage(backgroundPicture,backgroundX,backgroundY,canvasWidth,canvasHeight);
+  }
+}
+mc.strokeStyle = 'blue';
+mc.strokeRect(mcPositionX1, mcPositionY1, 50, 60);
 
+opp.strokeStyle = 'red';
+opp.strokeRect(opponentX1, opponentY1, 50, 60);
+let oppAttackTimeAmount = 2;
+function opponentHitbox() {
+  opp.strokeStyle = 'red';
+  opp.strokeRect(opponentX1, opponentY1, 50, 60);
+  if(
+    opponentX2>mcPositionX1 &&
+    opponentX1<mcPositionX2 &&
+    opponentY1<mcPositionY2 &&
+    opponentY2>mcPositionY1
+  ) {
+//this timer will give the opponent a certain amount of time before it attacks back
+  let oppAttackCountDown = setInterval(oppAttackTime, 1000);
+  function oppAttackTime() {
+    oppAttackTimeAmount -= 1;
+    mcHealth -= 1;
+    console.log(mcHealth)
+    if(oppAttackTimeAmount == 0) {
+      clearInterval(oppAttackCountDown);
+    }
+}
+}
+
+}
+let mainCharacter = new Image();
+mainCharacter.src = "images/sprites/characterMovementSprites.png";
+mainCharacter.onload = function drawBase() {
+mc.drawImage(mainCharacter,0,0,20,30,mcPositionX1,mcPositionY1,50,60);
+}
+//main characters design
+function mainDesignDown() {
+  let mainCharacter = new Image();
+  mainCharacter.src = "images/sprites/characterMovementSprites.png";
+  mainCharacter.onload = function drawBase() {
+  mc.drawImage(mainCharacter,0,0,20,30,mcPositionX1,mcPositionY1,50,60);
+  }
+}
+function mainDesignUp() {
+  let mainCharacter = new Image();
+  mainCharacter.src = "images/sprites/characterMovementSprites.png";
+  mainCharacter.onload = function drawBase() {
+  mc.drawImage(mainCharacter,0,37,20,30,mcPositionX1,mcPositionY1,50,60);
+  }
+}
+function mainDesignRight() {
+  let mainCharacter = new Image();
+  mainCharacter.src = "images/sprites/characterMovementSprites.png";
+  mainCharacter.onload = function drawBase() {
+  mc.drawImage(mainCharacter,0,72,20,30,mcPositionX1,mcPositionY1,50,60);
+  }
+}
+function mainDesignLeft() {
+  let mainCharacter = new Image();
+  mainCharacter.src = "images/sprites/characterMovementSprites.png";
+  mainCharacter.onload = function drawBase() {
+  mc.drawImage(mainCharacter,212,72,20,30,mcPositionX1,mcPositionY1,50,60);
+  }
+}
 //main characters hitbox
-function main() {
-  ctx.strokeStyle = 'blue';
-  ctx.strokeRect(midXCanvas-50, midYCanvas-50, 100, 100);
+function mainHitbox() {
+  mc.strokeStyle = 'blue';
+  mc.strokeRect(mcPositionX1, mcPositionY1, 50, 60);
 }
 //brings the inventory on screen
 function inventoryBox() {
-  ctx1.strokeStyle = 'red';
-  ctx1.strokeRect(midXCanvas-300, midYCanvas-200, 600, 400);
+  inv.strokeStyle = 'red';
+  inv.strokeRect(50, 30, 600, 400);
 }
 //movement keys
 function left() {
-ctx.clearRect(0,0,900,450);
-midXCanvas -= 10;
-main();
+  bg.clearRect(0,0,900,450);
+  mc.clearRect(0,0,900,450);
+  opp.clearRect(0,0,900,450);
+  backgroundX += 10;
+  opponentX1 += 10;
+  opponentX2 += 10;
+  mainHitbox();
+  mainDesignLeft();
+  background();
+  opponentHitbox();
 }
 function right() {
-ctx.clearRect(0,0,900,450);
-midXCanvas += 10;
-main();
+  bg.clearRect(0,0,900,450);
+  mc.clearRect(0,0,900,450);
+  opp.clearRect(0,0,900,450);
+  backgroundX -= 10;
+  opponentX1 -= 10;
+  opponentX2 -= 10;
+  mainHitbox();
+  mainDesignRight();
+  background();
+  opponentHitbox();
 }
 function up() {
-ctx.clearRect(0,0,900,450);
-midYCanvas -= 10;
-main();
+  bg.clearRect(0,0,900,450);
+  mc.clearRect(0,0,900,450);
+  opp.clearRect(0,0,900,450);
+  backgroundY += 10;
+  opponentY1 += 10;
+  opponentY2 += 10;
+  mainHitbox();
+  mainDesignUp();
+  background();
+  opponentHitbox();
 }
 function down() {
-ctx.clearRect(0,0,900,450);
-midYCanvas += 10;
-main();
+  bg.clearRect(0,0,900,450);
+  mc.clearRect(0,0,900,450);
+  opp.clearRect(0,0,900,450);
+  backgroundY -= 10;
+  opponentY1 -= 10;
+  opponentY2 -= 10;
+  mainHitbox();
+  mainDesignDown();
+  background();
+  opponentHitbox();
 }
 let inventoryCount = 0;
 //inventory function
-function inventory() {
-inventoryBox();
-inventoryCount += 1;
-if(inventoryCount%2 == 0) {
-ctx1.clearRect(0,0,900,450);
+function inventoryPopUp() {
+  inventoryBox();
+  inventoryCount += 1;
+  if(inventoryCount%2 == 0) {
+  inv.clearRect(0,0,900,450);
+}
+
+//fight function
+function fight() {
+  console.log("swish")
+  if(
+  opponentX1 == mcPositionX2+10 ||
+  opponentX2 == mcPositionX1 ||
+  opponentY1 == mcPositionY2 ||
+  opponentY2 == mcPositionY1
+) {
+  opponentHealth -= 1;
+  console.log(opponentHealth)
 }
 }
+}
+function fight() {
+  console.log("swish")
+  if(
+  opponentX1 == mcPositionX2 ||
+  opponentX2 == mcPositionX1 ||
+  opponentY1 == mcPositionY2 ||
+  opponentY2 == mcPositionY1
+) {
+  opponentHealth -= 1;
+  console.log(opponentHealth)
+}
+}
+
+
+
 //switch case for function keys
 window.addEventListener("keypress", (event) => {
     switch (event.key) {
@@ -66,7 +218,9 @@ window.addEventListener("keypress", (event) => {
             down();
             break;
         case "i":
-            inventory();
+            inventoryPopUp();
             break;
+        case "e":
+            fight();
     }
 });
